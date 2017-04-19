d41758f5443116d368e176556a537d3bdd22c80a
Anuraag Yachamaneni <ayachama@purdue.edu>
2017-04-19 13:52:02 -0400

    Fix login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, :skip => [:passwords, :sessions, :registrations]
 
   devise_scope :user do
-    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    get 'login', :to => 'dashboard#index', :as => :new_user_session
+    get 'logout', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
   root "dashboard#index"
