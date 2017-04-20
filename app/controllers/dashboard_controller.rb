7718fce43f7a2823e42c071148df751108c1b57c
Anuraag Yachamaneni <ayachama@purdue.edu>
2017-04-20 02:42:35 -0400

    Finished building simple introduction form

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -15,7 +15,21 @@ class DashboardController < ApplicationController
   end
 
   def update_info
-    
-    render :index
+    current_user.assign_attributes(:dob              => params[:dob],
+                                   :bio              => params[:bio],
+                                   :height           => "#{params[:feet]}-#{params[:inches]}",
+                                   :starting_weight  => params[:weight],
+                                   :goal_weight      => params[:goal_weight],
+                                   :body_fat         => params[:bf],
+                                   :activity_level   => params[:activity_level],
+                                   :program_type     => params[:program_type],
+                                   :goal_body_fat    => params[:goal_bf],
+                                   :program_length   => params[:program_length],
+                                   :entered_info     => true )
+    if current_user.save
+      redirect_to root_path
+    else
+      render :index, :notice => current_user.errors
+    end
   end
 end
